d19cb0237a5e3fec55659cebe8136b2724420bf1
Nicole Zheng <zznicole@hotmail.com>
2020-11-13 16:11:42 +0100

    setup LoginScreen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import { FormControl, Container, TextField } from '@material-ui/core';
+import { FormControl, Container, TextField, Button, TextareaAutosize } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => {
+const useStyles = makeStyles((theme) => ({
+  screen: {
+    backgroundColor: '#00bcd4',
+    // backgroundImage: "url'./assets/OSL-bg.png'",
+    backgroundSize: 'cover',
+    height: '100vh',
+    display: 'flex',
+  },
 
-})
+  form: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+  }
+}));
+
 export default function LoginForm() {
   const classes = useStyles();
   return (
-    <Container>
-      <form>
+    <Container className={classes.screen}>
+      <form className={classes.form}>
         <FormControl>
-          <TextField required={true} variant='standard' label="Enter your Username or Email" floatingLabelText="Username" />
+          <TextField type="Email" required variant='standard' size="medium" label="Email" floatingLabelText="Email" />
           <br />
-          <TextField />
+          <TextField type="password" required variant='standard' label="Password" floatingLabelText="Password" />
           <br />
-          <Button variant="contained" color="">LOG IN</Button>
+          <Button variant="contained" color="primary">LOG IN</Button>
+          <br />
+          <br />
+          <p>Not signed up yet? Sign up now!</p>
+          <Button variant="contained" color="primary">SIGN UP</Button>
         </FormControl>
       </form>
-      <p></p>
-      <Button variant="contained" color="">SIGN UP</Button>
+      
     </Container>
 
   );
